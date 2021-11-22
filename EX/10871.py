ba93a3b37584a24abbb073a77fee312283881779
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2021-11-22 18:36:39 +0900

    2021.11.22
    .

--- EX/10871.py
+++ EX/10871.py
@@ -3,7 +3,7 @@ import sys
 N, X = map(int,sys.stdin.readline().split())
 A = list(map(int,sys.stdin.readline().split()))
 
-for i in A:
-    a = A*2
-    print(a)
+for i in range(N):
+    if A[i] < X:
+        print(A[i], end=" ")
 
---
+++ EX/10952.py
@@ -0,0 +1,6 @@
+import sys
+a, b = map(int, sys.stdin.readline().split())
+sum = a+b
+while sum == 0:
+    
+    print(sum)
