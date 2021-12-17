33766ff30cab53caa744a1ae11788031c02daea5
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2021-12-17 21:25:07 +0900

    2021.12.17

--- 기본수학/2775.py
+++ 기본수학/2775.py
@@ -1,12 +1,12 @@
 T = int(input())
-human = 1
-def apart(floor, room):
-    pass 
 
-
-for i in range(T):
+for _ in range(T):
     k = int(input())
     n = int(input())
-"""1,3,6,10
-    1,4,10,20
-    1,5,1,35"""
+    apart = [i for i in range(1, n+1)]
+
+    for i in range(k):
+        for ii in range(1, n):
+            apart[ii] += apart[ii-1]  
+
+    print(apart[-1])
---
+++ 기본수학/rc.py
@@ -0,0 +1,4 @@
+room = [[0]*3 for i in range(1, 4)]
+room1 = [[0]*3 for i in range(3)]
+print(room)
+print(room1)
