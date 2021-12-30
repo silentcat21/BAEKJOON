817403c80266689b10c376af8ab099c64a1aa47d
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2021-12-30 15:56:34 +0900

    2021.12.30

--- 브루트 포스/2798.py
+++ 브루트 포스/2798.py
@@ -2,5 +2,18 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 cardlist = list(map(int, sys.stdin.readline().split()))
+sum = 0
 
-print(cardlist)
+for i in range(0, N-2):
+    for ii in range(i+1, N-1):
+        for iii in range(ii+1, N):
+            if cardlist[i] + cardlist[ii] + cardlist[iii] > M:
+                continue
+            else:
+                sum = max(sum, cardlist[i] + cardlist[ii] + cardlist[iii])
+
+print(sum)
+
+
+    
+
